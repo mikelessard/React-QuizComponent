c8d1e851e0b79e7b22c8832e7cb65ae2519feee8
Lessard, Michael <Michael.Lessard@LibertyMutual.com>
2019-02-17 10:48:27 -0500

    make it match answer

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -40,9 +40,8 @@ class QuizQuestion extends Component {
             <section className="buttons">
               <ul>
                 {this.props.quiz_question.answer_options.map((answer_option, index) => {
-                    return (
-                        <QuizQuestionButton button_text={answer_option} key={index} clickHandler={this.handleClick.bind(this)}/>
-                    )})
+                    return <QuizQuestionButton button_text={answer_option} key={index} clickHandler={this.handleClick.bind(this)}/>
+                })
                 }
              </ul>
             </section>
