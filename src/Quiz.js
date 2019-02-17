dfb6af0475352017745977b2a094f5704bdcc282
Lessard, Michael <Michael.Lessard@LibertyMutual.com>
2019-02-17 11:05:20 -0500

    again use reg method

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,7 +12,7 @@ class Quiz extends Component {
         this.state = {quiz_position: 1};
     }
 
-    showNextQuestion = () => {
+    showNextQuestion () {
         this.setState((state) => {
             return {quiz_position: state.quiz_position + 1}
         })
